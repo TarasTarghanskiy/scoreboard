5114a71c5fe9bab10e8e9edbd920a7525e3cfb06
taras.t <neWsM@keR!1>
2025-05-08 20:15:55 +0000

    feat: ScoreboardService.java

---
+++ src/main/java/com/tarhanskyi/ScoreboardService.java
@@ -0,0 +1,32 @@
+package com.tarhanskyi;
+
+
+
+//The scoreboard supports the following operations:
+//1. Start a new match, assuming initial score 0 – 0 and adding it the scoreboard.
+// This should capture following parameters:
+//  a. Home team
+//  b. Away team
+//2. Update score. This should receive a pair of absolute scores: home team score and away
+//team score. (id?)
+//        3. Finish match currently in progress. This removes a match from the scoreboard.
+//4. Get a summary of matches in progress ordered by their total score. The matches with the
+//same total score will be returned ordered by the most recently started match in the
+//scoreboard.
+
+// Keep it simple
+
+// Do not forget about edge cases!
+
+// Just a simple library implementation.
+// Focus on Quality.
+// Use Test-Driven Development (TDD)
+// pay attention to: OO design, Clean Code and adherence to SOLID principles.
+// Please share your solution with a link to a source control repository (e.g. GitHub, GitLab, BitBucket)
+// commit history is important
+public class ScoreboardService {
+}
+
+
+//Add a README.md file where beside the project documentation you can make notes of any
+//assumption or things you would like to mention to us about your solution.

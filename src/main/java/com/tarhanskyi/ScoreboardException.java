18c0b7f9d58dbe3fe83b38c1b4ae0e22e97ab5fe
taras.t <neWsM@keR!1>
2025-05-09 07:29:45 +0000

    feat: add static factory methods for exceptions

--- src/main/java/com/tarhanskyi/ScoreboardException.java
+++ src/main/java/com/tarhanskyi/ScoreboardException.java
@@ -1,7 +1,22 @@
 package com.tarhanskyi;
 
+import java.util.UUID;
+
 public class ScoreboardException extends RuntimeException {
-    public ScoreboardException(String message) {
+    private ScoreboardException(String message) {
         super(message);
+    }
+
+    static void matchExistsFor(String teamName) {
+        throw new ScoreboardException("Match already exists for a team: " + teamName);
+    }
+
+    static void notFoundById(UUID id, boolean isNotFound) {
+        if (isNotFound) throw new ScoreboardException("Match not found with ID: " + id);
+    }
+
+    static void matchLimitReached(int activeMatchesCount) {
+        if (activeMatchesCount == 100)
+            throw new ScoreboardException("Too many active matches, current: " + activeMatchesCount + ", limit: 100");
     }
 }
